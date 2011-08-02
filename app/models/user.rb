1a770cd5c507792aacb178a7a3c7e25fb7756f3e
Kevin Ross <kevin.ross@metova.com>
2011-08-02 17:00:58 -0500

    Initial commit, converting to rails 3.1 rc5
    Toying with the idea of using the devise/omniauth combination, so I figured I would update the example as a decent opportunity to explore the solution...

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ActiveRecord::Base
                             :small => "100x100#",
                             :big => "150x150#"
                     },
-                    :default_url => "/images/user_photos/missing_:style.png"
+                    :default_url => "user_photos/missing_:style.png"
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
